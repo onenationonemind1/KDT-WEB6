0c273864c1f221c2e585786086a1b23137f44f3e
onenationonemind1 <rkfmvk1117@gmail.com>
2023-03-28 17:52:11 +0900

    study: async / await - practice 3

--- 07_async/practice4.js
+++ 07_async/practice4.js
@@ -1,42 +1,55 @@
 // 실습2. Promise -> async/await 기본코드
 // call, back, hell 함수: 실습 23의 함수와 동일
-function call(name) {
+// function call(name) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       console.log(name);
+//       resolve(name); // 작업 성공시 then(name)
+//     }, 1000);
+//   });
+// }
+
+// function back() {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       console.log("back");
+//       resolve("back"); // 작업 성공시 then('back')
+//     }, 1000);
+//   });
+// }
+
+// function hell() {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       resolve("callback hell");
+//     }, 1000);
+//   });
+// }
+
+// async function run() {
+//   try {
+//     const name = await call("John");
+//     console.log(name + " 반가워");
+//     const back1 = await back();
+//     console.log(back1 + "을 실행했구나.");
+//     const result = await hell();
+//     console.log("여기는 " + result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// run();
+
+function changeBgColor(color) {
   return new Promise(function (resolve, reject) {
     setTimeout(function () {
-      console.log(name);
-      resolve(name); // 작업 성공시 then(name)
+      document.body.style.backgroundColor = color;
+      resolve(color);
     }, 1000);
   });
 }
 
-function back() {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      console.log("back");
-      resolve("back"); // 작업 성공시 then('back')
-    }, 1000);
-  });
-}
-
-function hell() {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      resolve("callback hell");
-    }, 1000);
-  });
-}
-
-async function run() {
-  try {
-    const name = await call("John");
-    console.log(name + " 반가워");
-    const back1 = await back();
-    console.log(back1 + "을 실행했구나.");
-    const result = await hell();
-    console.log("여기는 " + result);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-run();
+changeBgColor("red").then(function () {
+  changeBgColor("blue");
+});
