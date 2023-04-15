2cc09d33a15a166ac377c4bfca7273f289dce074
onenationonemind1 <rkfmvk1117@gmail.com>
2023-04-15 15:43:44 +0900

    project

---
+++ 16_socket_practice_origin/public/client.js
@@ -0,0 +1,156 @@
+// public/client.js
+
+const socket = io();
+const messageForm = document.getElementById("message-form");
+const messageInput = document.getElementById("message-input");
+const messages = document.getElementById("messages");
+const nickname = prompt("Enter your nickname");
+const itsMe = nickname;
+const messageList = document.getElementById("who");
+let nickBox = [];
+let messageBox = [];
+let testBox = [];
+let flag = 0;
+
+if (nickname === "") {
+  console.log("beam");
+  alert("아이디 값을 입력해 주세요.");
+  window.location.reload();
+}
+
+if (nickname !== null) {
+  // user pressed "OK" button
+  console.log("coclco", nickname);
+
+  socket.emit("enter-message", { nickname });
+} else {
+  // user pressed "Cancel" button
+  console.log("User canceled the prompt.");
+}
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  if (message.trim() !== "") {
+    socket.emit("chat-message", { nickname, message });
+    messageInput.value = "";
+  }
+});
+
+messageForm.addEventListener("submit", (e) => {
+  const message = messageInput.value;
+  socket.emit("enter-message", { nickname, message });
+});
+
+function addMessageToPage(nickname, message, isMyMessage) {
+  const messageElement = document.createElement("li");
+  if (itsMe === nickname) {
+    // document.getElementById("who").innerHTML = `${nickname}님 환영합니다!!!`;
+  }
+
+  // messageElement.innerHTML = `<strong class="msg">${nickname}:</strong> <span class="msg">${message}</span>`;
+  messageElement.innerHTML = `<strong> ${nickname} </strong> :  ${message}`;
+
+  messageElement.classList.add("message");
+  if (itsMe === nickname) {
+    messageElement.classList.add("my-message");
+  } else {
+    messageElement.classList.add("other-message");
+  }
+  messages.appendChild(messageElement);
+  // messages.appendChild(messageElement);
+  messages.scrollTop = messages.scrollHeight;
+}
+
+function chatMessageToPage(txt) {
+  const messageElement = document.createElement("li");
+  if (itsMe === nickname) {
+    // document.getElementById("who").innerHTML = `${nickname}님 환영합니다!!!`;
+  }
+
+  // messageElement.innerHTML = `<strong class="msg">${nickname}:</strong> <span class="msg">${message}</span>`;
+  messageElement.innerHTML = `<strong> ${txt} </strong> : 포인트가 1점 상승했습니다.`;
+
+  messageElement.classList.add("chat-message");
+  messages.appendChild(messageElement);
+  // messages.appendChild(messageElement);
+  messages.scrollTop = messages.scrollHeight;
+}
+
+function enterMessageToPage(nickname) {
+  const messageElement = document.createElement("li");
+  if (itsMe === nickname) {
+    // document.getElementById("who").innerHTML = `${nickname}님 환영합니다!!!`;
+  }
+
+  messageElement.innerHTML = `<strong> ${nickname} </strong> 님이 입장하셨습니다. `;
+
+  messageElement.classList.add("message-enter");
+  messageElement.classList.add("my-message-enter");
+
+  messages.appendChild(messageElement);
+
+  messages.scrollTop = messages.scrollHeight;
+}
+
+function listToPage(txt) {
+  const messageElement = document.createElement("li");
+
+  messageElement.innerHTML = `<strong> ${txt} </strong> `;
+
+  messageElement.classList.add("message-enter");
+  messageElement.classList.add("my-message-enter");
+
+  messageList.appendChild(messageElement);
+}
+
+socket.on("enter-message", ({ nickname }) => {
+  nickBox.push(nickname);
+  let count = nickBox.filter((item) => item === nickname).length;
+  console.log(count);
+
+  if (count === 1) {
+    enterMessageToPage(nickname);
+  }
+});
+
+socket.on("chat-message", ({ nickname, message }) => {
+  console.log(nickname, message);
+  addMessageToPage(nickname, message, true);
+});
+
+socket.on("openai", ({ message }) => {
+  addMessageToPage(message, message, true);
+  console.log(message);
+});
+
+socket.on("disconnect", ({ nickname }) => {
+  console.log("A user disconnected");
+});
+
+//chat gpt
+socket.on("chatGptAns", (result) => {
+  let result_str = result;
+  result_str = result_str.replace(/\s/g, "");
+  console.log(result_str, "@@@@@@@@@@");
+  listToPage(result_str);
+  chatMessageToPage(result);
+});
+
+document.getElementById("message-input") = function () {
+  const msg = document.getElementById("message-input").value;
+  socket.emit("client_msg", msg);
+
+  //-------
+  messageElement = document.createElement("li");
+  messageElement.innerHTML = `<strong"> ${msg} </strong>`;
+  messageElement.classList.add("message");
+  if (itsMe === nickname) {
+    messageElement.classList.add("my-message");
+  } else {
+    messageElement.classList.add("other-message");
+  }
+  messages.appendChild(messageElement);
+  // messages.appendChild(messageElement);
+  messages.scrollTop = messages.scrollHeight;
+};
---
+++ 16_socket_practice_origin/server.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+let messageBox = [];
+
+/////////////////////////////////
+
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion(txt) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: txt,
+  });
+  console.log("#########", completion.data.choices[0].text);
+
+  let a = completion.data.choices[0].text;
+  return a;
+}
+////////////////////////////////
+
+// Set the view engine to ejs
+app.set("view engine", "ejs");
+
+// Serve static files from the public directory
+app.use(express.static("public"));
+
+// Serve the index page
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Listen for socket connections
+io.on("connect", (socket) => {
+  console.log(socket.id);
+  io.on("connection", (socket) => {
+    console.log(socket.id);
+  });
+  // console.log("A user connected   ID :", `${socket.id}`);
+
+  socket.on("enter-message", ({ nickname }) => {
+    console.log(`enter-message ${nickname}`);
+    io.emit("enter-message", { nickname });
+  });
+
+  // Listen for chat messages
+  socket.on("chat-message", ({ nickname, message }) => {
+    console.log(`chat-message- ${nickname}: ${message}`);
+    messageBox.push(message);
+    console.log(messageBox, messageBox.length);
+    if (messageBox.length === 4) {
+      messageBox.join(",");
+      console.log(messageBox);
+      let result = runCompletion(
+        `Summarize the topic of this conversation and answer in one word. ${messageBox}`
+      )
+        .then((result) => {
+          console.log("11111111", result, "2222");
+          console.log("result", result, "3333");
+          io.emit("chatGptAns", result);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+      console.log("11111111", result);
+      io.emit("chatGptAns", result);
+      messageBox = [];
+    }
+    io.emit("chat-message", { nickname, message });
+  });
+
+  // OpenAi
+  socket.on("openai", ({ message }) => {
+    console.log(`${message}`);
+    io.emit("openai", { message });
+  });
+
+  // Disconnect event
+  socket.on("disconnect", (nickname) => {
+    console.log("A user disconnected");
+  });
+});
+
+// Start the server
+http.listen(3002, () => {
+  console.log("Server started on port 3000");
+});
