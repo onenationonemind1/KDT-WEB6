1ed9b69b1e7c7d55371116bf0980f7bd86261e1c
onenationonemind1 <rkfmvk1117@gmail.com>
2023-04-24 18:57:49 +0900

    study: 25_REACT_07_HOOKS

---
+++ 25_react_07_hooks/src/App.js
@@ -0,0 +1,15 @@
+import UseMemoTest from "./UseMemoTest";
+import UseCallbackTest from "./UseCallbackTest";
+import UseReducerTest from "./UseReducerText";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UseMemoTest /> */}
+      {/* <UseCallbackTest /> */}
+      <UseReducerTest />
+    </div>
+  );
+}
+
+export default App;
