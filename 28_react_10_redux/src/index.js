48d2a726e1a97b3589d02bf9529ef51e0ca08ad8
onenationonemind1 <rkfmvk1117@gmail.com>
2023-05-01 17:33:13 +0900

    study: 28_react_10_redux

--- 28_react_10_redux/src/index.js
+++ 28_react_10_redux/src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import App2 from "./App2";
+import App4 from "./App4";
 import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./store";
 
 // state 초기값 설정
 const initialState = {
@@ -25,13 +26,13 @@ const reducer = (state = initialState, action) => {
 
 // store 정의 : 상태를 관리하는 공간!
 // 단, 하나의 프로젝트에는 하나의 store만 있어야 함!
-const store = configureStore({ reducer: reducer }, composeWithDevTools());
+const store = configureStore({ reducer: rootReducer }, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // Provider 컴포넌트: store props를 반드시 가져야 함
   // Provider 하위 컴포넌트들은 store에 저장된 state들을 사용 가능!
   <Provider store={store}>
-    <App2 />
+    <App4 />
   </Provider>
 );
